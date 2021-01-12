779e93d7d372394f3a88ad9029b335cd18f9dc94
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-01-12 01:30:46 +0000

    Added user inputs

--- ratings.py
+++ ratings.py
@@ -4,7 +4,10 @@
 define a function: provide_ratings(text_file):
 create: restaurant_dict = {}
 open text file: rest_file = open('scores.txt')
-
+create a variable (restaurant_name), equal to input(ask for retaurant name)
+create a variable (restaurant_score), equal to input(ask for a score)
+add restaurant_name and restaurant_score to restaurant_dict
+    restaurant_dict[restaurant_name] = restaurant_score
 itertate through file line by line using for loop
     create a variable restaurant_ratings = rstrip().split(':') 
         populate restaurant_dict with [restaurant_ratings[0] = restaurant_ratings[1]
@@ -18,6 +21,9 @@ def provide_ratings(text_file):
 
     restaurant_dict = {}
 
+    restaurant_name = input("What is the name of the restaurant? >")
+    restaurant_score = input("What score do you give the restaurant? >")
+    restaurant_dict[restaurant_name] = restaurant_score
 
     for line in text_file:
         restaurant_ratings = line.rstrip().split(':')
